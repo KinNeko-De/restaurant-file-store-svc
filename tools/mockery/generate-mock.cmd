aeff5691e9661149e3f91d855701c15051b6f9a6
Nils Diekmann <github@kinneko.de>
2024-02-18 17:52:01 +0100

    - adapt script to new mock location

--- tools/mockery/generate-mock.cmd
+++ tools/mockery/generate-mock.cmd
@@ -1,6 +1,6 @@
 pushd windows_x86_64
 
-mockery --dir "%GOPATH%/pkg/mod/github.com/kinneko-de/api-contract/golang/kinnekode/restaurant@v0.0.2-store-files.16/file/v1" --name FileService_StoreFileServer --filename file_service_store_file_server_mock.go --with-expecter --output ../../../internal/app/file --outpkg file
+mockery --dir "%GOPATH%/pkg/mod/github.com/kinneko-de/api-contract/golang/kinnekode/restaurant@v0.0.2-store-files.16/file/v1" --name FileService_StoreFileServer --filename file_service_store_file_server_mock.go --with-expecter --output ../../../test/testing/file --outpkg file
 mockery --dir "../../../internal/app/file" --name FileMetadataRepository --filename file_metadata_repository_mock.go --with-expecter --inpackage
 mockery --dir "../../../internal/app/file" --name FileRepository --filename file_repository_mock.go --with-expecter --inpackage
 mockery --dir ".." --name WriteCloser --structname MockWriteCloser --filename write_closer_mock.go --with-expecter --output ../../../test/testing/io --outpkg io
